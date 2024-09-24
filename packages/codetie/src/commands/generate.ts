55c7f3ec3123debc9a9ec2d666a8c4d454162fa8
Tim Kernegger <tim@macherjek.at>
2024-09-24 16:32:25 +0200

    clear unused statements

--- packages/codetie/src/commands/generate.ts
+++ packages/codetie/src/commands/generate.ts
@@ -1,32 +1,35 @@
-import { defineCommand } from "citty";
-import { resolve,  } from 'pathe'
-import { exists } from '../utils/fs'
+import { defineCommand } from 'citty'
+import { resolve } from 'pathe'
 import { consola } from 'consola'
-import {$} from 'execa';
+import { $ } from 'execa'
 
 export default defineCommand({
   meta: {
-    name: "generate",
-    description: "Generate the project",
+    name: 'generate',
+    description: 'Generate the project',
   },
   args: {
     cwd: {
       type: 'string',
       description: 'The directory to generate the project in',
-    }
+    },
   },
   async run(ctx) {
     const argsCwd = typeof ctx.args.cwd === 'boolean' ? undefined : ctx.args.cwd
     const cwd = resolve(argsCwd || '.')
-    
+
     try {
       const { stdout } = await $`command -v xcodegen &> /dev/null`
       if (!stdout) {
         throw new Error()
       }
     } catch (error) {
-      consola.error('Error: XcodeGen is not installed. Please install XcodeGen before running this script.')
-      consola.box('You can install XcodeGen using Homebrew:\n\nbrew install xcodegen')
+      consola.error(
+        'Error: XcodeGen is not installed. Please install XcodeGen before running this script.'
+      )
+      consola.box(
+        'You can install XcodeGen using Homebrew:\n\nbrew install xcodegen'
+      )
       process.exit(1)
     }
 
@@ -39,5 +42,5 @@ export default defineCommand({
       consola.error(error)
       process.exit(1)
     }
-  }
+  },
 })
